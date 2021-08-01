83f20d4ca3fff6ca907e8b9383c7f48c4f8ad8b8
win11264 <winthitisan@gmail.com>
2021-08-02 00:19:49 +0700

    Lab 2 commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 
-import "./component/Lab1.css";
+import LabList from "./component/lab_2_list";
 
 function App() {
   return (
-    <div className="Lab1">
-      <button className="button">+</button>
-      <p>0</p>
-      <button className="button">-</button>
-      <button className="button">reset</button>
+    <div className="Lab2">
+      <button className="button">Add</button>
+      <LabList />
     </div>
   );
 }
